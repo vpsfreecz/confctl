38e925d148fc99be4831e9387460bf5afc6b8290
Jakub Skokan <jakub.skokan@havefun.cz>
2020-11-07 08:51:03 +0100

    example: more examples in shell.nix

--- example/shell.nix
+++ example/shell.nix
@@ -1,1 +1,11 @@
+# When a part of confctl repository
 import ../shell.nix
+
+# When copied elsewhere
+# if builtins.pathExists ../confctl/shell.nix then
+#   import ../confctl/shell.nix
+# else if builtins.pathExists ../../confctl/shell.nix then
+#   import ../../confctl/shell.nix
+# else if builtins.pathExists /where-is-your-confctl/shell.nix then
+#   import /where-is-your-confctl/shell.nix
+# else builtins.abort "Unable to find confctl shell"
