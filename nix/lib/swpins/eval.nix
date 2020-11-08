926899952d46bcaab5bfde20ae881066e4edde91
Jakub Skokan <jakub.skokan@havefun.cz>
2020-11-08 19:17:58 +0100

    git-rev: revision -> git-<revision>

--- nix/lib/swpins/eval.nix
+++ nix/lib/swpins/eval.nix
@@ -45,7 +45,7 @@ let
         repo = fetchers.${opts.wrapped_fetcher.type} opts.wrapped_fetcher.options;
         shortRev = lib.substring 0 7 (opts.rev);
       in
-        pkgs.runCommand "revision" {} ''
+        pkgs.runCommand "git-${shortRev}" {} ''
           cp -a ${repo} $out
           chmod 700 $out
           echo "${shortRev}" > $out/.git-revision
