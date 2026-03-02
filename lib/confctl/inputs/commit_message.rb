c9fac08a2506b10ec13f067e41dd79951f976518
Jakub Skokan <jakub.skokan@havefun.cz>
2026-03-02 10:31:16 +0100

    inputs: include revision in commit title

--- lib/confctl/inputs/commit_message.rb
+++ lib/confctl/inputs/commit_message.rb
@@ -6,9 +6,7 @@ module ConfCtl
       def self.build(changes:, changelog:, downgrade: false, action: :update)
         raise ArgumentError, 'no changes' if changes.nil? || changes.empty?
 
-        names = changes.map(&:name).sort
-        verb = action == :set ? 'set' : 'update'
-        title = "inputs: #{verb} #{names.join(', ')}"
+        title = build_title(changes, action)
 
         body = +''
 
@@ -34,6 +32,41 @@ module ConfCtl
 
         "#{title}\n\n#{body}".strip
       end
+
+      def self.build_title(changes, action)
+        names = changes.map(&:name).sort
+        verb = action == :set ? 'set' : 'update'
+        title = "inputs: #{verb} #{names.join(', ')}"
+        short_rev = common_short_rev(changes)
+
+        short_rev ? "#{title} to #{short_rev}" : title
+      end
+
+      def self.common_short_rev(changes)
+        reference = changes.first
+
+        return nil if reference.new_rev.nil? || reference.new_short_rev.nil?
+        return nil if changes.any? { |ch| ch.new_rev != reference.new_rev }
+        return reference.new_short_rev if changes.length == 1
+        return nil unless same_sources?(changes, reference.url)
+
+        reference.new_short_rev
+      end
+
+      def self.same_sources?(changes, reference_url)
+        normalized_reference = normalize_url(reference_url)
+        return false if normalized_reference.nil?
+
+        changes.all? { |ch| normalize_url(ch.url) == normalized_reference }
+      end
+
+      def self.normalize_url(url)
+        return nil if url.nil?
+
+        url.sub(%r{/+\z}, '')
+      end
+
+      private_class_method :build_title, :common_short_rev, :same_sources?, :normalize_url
     end
   end
 end
