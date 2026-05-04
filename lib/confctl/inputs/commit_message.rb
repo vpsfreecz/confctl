885e5f38431a74621aef5fb0d4eb399c677df608
Jakub Skokan <jakub.skokan@havefun.cz>
2026-05-04 18:15:36 +0200

    inputs: group shared changelogs
    Group inputs that update the same source from the same old revision
    to the same new revision when building changelog commit messages.
    This avoids duplicating identical git logs in commits generated by
    inputs update.
    
    Render changelog groups with a blank line between them and keep
    --no-changelog messages compact. Add unit coverage for grouped logs,
    separate old revisions, and unchanged no-changelog formatting.

--- lib/confctl/inputs/commit_message.rb
+++ lib/confctl/inputs/commit_message.rb
@@ -8,29 +8,80 @@ module ConfCtl
 
         title = build_title(changes, action)
 
+        body =
+          if changelog
+            build_changelog_body(changes, downgrade)
+          else
+            build_plain_body(changes)
+          end
+
+        "#{title}\n\n#{body}".strip
+      end
+
+      def self.build_plain_body(changes)
+        changes.each_with_object(+'') do |ch, body|
+          body << change_summary(ch)
+        end
+      end
+
+      def self.build_changelog_body(changes, downgrade)
+        group_changes(changes).map do |group|
+          build_changelog_group(group, downgrade).rstrip
+        end.join("\n\n")
+      end
+
+      def self.build_changelog_group(group, downgrade)
         body = +''
 
+        group.each do |ch|
+          body << change_summary(ch)
+        end
+
+        reference = group.first
+        return body unless reference.url && reference.old_rev && reference.new_rev
+        return body if reference.old_rev == reference.new_rev
+
+        from = downgrade ? reference.new_rev : reference.old_rev
+        to = downgrade ? reference.old_rev : reference.new_rev
+
+        begin
+          mirror = ConfCtl::GitRepoMirror.new(reference.url, quiet: true)
+          mirror.setup
+          body << mirror.log(from, to, opts: ['--oneline'])
+        rescue StandardError => e
+          body << "(changelog unavailable: #{e})"
+        end
+
+        body
+      end
+
+      def self.group_changes(changes)
+        groups = []
+        group_indexes = {}
+
         changes.each do |ch|
-          body << "#{ch.name}: #{ch.old_rev ? ch.old_short_rev : 'set'} -> #{ch.new_short_rev || '-'}\n"
+          key = group_key(ch)
 
-          next unless changelog
-          next unless ch.url && ch.old_rev && ch.new_rev
-          next if ch.old_rev == ch.new_rev
-
-          from = downgrade ? ch.new_rev : ch.old_rev
-          to = downgrade ? ch.old_rev : ch.new_rev
-
-          begin
-            mirror = ConfCtl::GitRepoMirror.new(ch.url, quiet: true)
-            mirror.setup
-            body << mirror.log(from, to, opts: ['--oneline'])
-            body << "\n"
-          rescue StandardError => e
-            body << "(changelog unavailable: #{e})\n\n"
+          if key && group_indexes.has_key?(key)
+            groups[group_indexes[key]] << ch
+          else
+            group_indexes[key] = groups.length if key
+            groups << [ch]
           end
         end
 
-        "#{title}\n\n#{body}".strip
+        groups
+      end
+
+      def self.group_key(change)
+        url = normalize_url(change.url)
+        return nil if url.nil?
+
+        [url, change.old_rev, change.new_rev]
+      end
+
+      def self.change_summary(change)
+        "#{change.name}: #{change.old_rev ? change.old_short_rev : 'set'} -> #{change.new_short_rev || '-'}\n"
       end
 
       def self.build_title(changes, action)
@@ -66,7 +117,9 @@ module ConfCtl
         url.sub(%r{/+\z}, '')
       end
 
-      private_class_method :build_title, :common_short_rev, :same_sources?, :normalize_url
+      private_class_method :build_plain_body, :build_changelog_body, :build_changelog_group,
+                           :group_changes, :group_key, :change_summary, :build_title,
+                           :common_short_rev, :same_sources?, :normalize_url
     end
   end
 end
--- spec/inputs/commit_message_spec.rb
+++ spec/inputs/commit_message_spec.rb
@@ -12,6 +12,14 @@ RSpec.describe ConfCtl::Inputs::CommitMessage do
     )
   end
 
+  def expect_git_log(url:, from:, to:, output:)
+    mirror = instance_double(ConfCtl::GitRepoMirror)
+
+    expect(ConfCtl::GitRepoMirror).to receive(:new).with(url, quiet: true).and_return(mirror)
+    expect(mirror).to receive(:setup)
+    expect(mirror).to receive(:log).with(from, to, opts: ['--oneline']).and_return(output)
+  end
+
   it 'includes the new revision in title for a single input' do
     changes = [
       build_change(
@@ -46,6 +54,117 @@ RSpec.describe ConfCtl::Inputs::CommitMessage do
     title = described_class.build(changes: changes, changelog: false).lines.first.chomp
 
     expect(title).to eq('inputs: update nixpkgsStable, nixpkgsUnstable to 22222222')
+  end
+
+  it 'keeps no-changelog input summaries compact' do
+    changes = [
+      build_change(
+        name: 'nixpkgsStable',
+        old_rev: '1111111111111111111111111111111111111111',
+        new_rev: '2222222222222222222222222222222222222222',
+        url: 'https://github.com/NixOS/nixpkgs'
+      ),
+      build_change(
+        name: 'nixpkgsUnstable',
+        old_rev: '3333333333333333333333333333333333333333',
+        new_rev: '2222222222222222222222222222222222222222',
+        url: 'https://github.com/NixOS/nixpkgs'
+      )
+    ]
+
+    message = described_class.build(changes: changes, changelog: false)
+
+    expect(message).to eq(<<~MSG.strip)
+      inputs: update nixpkgsStable, nixpkgsUnstable to 22222222
+
+      nixpkgsStable: 11111111 -> 22222222
+      nixpkgsUnstable: 33333333 -> 22222222
+    MSG
+  end
+
+  it 'groups changelog for inputs with the same source and same revision change' do
+    old_rev = '1111111111111111111111111111111111111111'
+    new_rev = '2222222222222222222222222222222222222222'
+    url = 'https://github.com/vpsfreecz/vpsadminos'
+    log = <<~LOG.strip
+      git log for #{old_rev}..#{new_rev}
+      > 222222222 shared change
+    LOG
+
+    changes = [
+      build_change(
+        name: 'vpsadminosStaging',
+        old_rev: old_rev,
+        new_rev: new_rev,
+        url: url
+      ),
+      build_change(
+        name: 'vpsadminosProduction',
+        old_rev: old_rev,
+        new_rev: new_rev,
+        url: "#{url}/"
+      )
+    ]
+
+    expect_git_log(url: url, from: old_rev, to: new_rev, output: log)
+
+    message = described_class.build(changes: changes, changelog: true)
+
+    expect(message).to eq(<<~MSG.strip)
+      inputs: update vpsadminosProduction, vpsadminosStaging to 22222222
+
+      vpsadminosStaging: 11111111 -> 22222222
+      vpsadminosProduction: 11111111 -> 22222222
+      git log for #{old_rev}..#{new_rev}
+      > 222222222 shared change
+    MSG
+  end
+
+  it 'keeps separate changelog groups for inputs with different old revisions' do
+    old_staging_rev = '1111111111111111111111111111111111111111'
+    old_os_staging_rev = '3333333333333333333333333333333333333333'
+    new_rev = '2222222222222222222222222222222222222222'
+    url = 'https://github.com/vpsfreecz/vpsadminos'
+    staging_log = <<~LOG.strip
+      git log for #{old_staging_rev}..#{new_rev}
+      > 222222222 staging change
+    LOG
+    os_staging_log = <<~LOG.strip
+      git log for #{old_os_staging_rev}..#{new_rev}
+      > 222222222 os staging change
+    LOG
+
+    changes = [
+      build_change(
+        name: 'vpsadminosStaging',
+        old_rev: old_staging_rev,
+        new_rev: new_rev,
+        url: url
+      ),
+      build_change(
+        name: 'vpsadminosProduction',
+        old_rev: old_os_staging_rev,
+        new_rev: new_rev,
+        url: url
+      )
+    ]
+
+    expect_git_log(url: url, from: old_staging_rev, to: new_rev, output: staging_log)
+    expect_git_log(url: url, from: old_os_staging_rev, to: new_rev, output: os_staging_log)
+
+    message = described_class.build(changes: changes, changelog: true)
+
+    expect(message).to eq(<<~MSG.strip)
+      inputs: update vpsadminosProduction, vpsadminosStaging to 22222222
+
+      vpsadminosStaging: 11111111 -> 22222222
+      git log for #{old_staging_rev}..#{new_rev}
+      > 222222222 staging change
+
+      vpsadminosProduction: 33333333 -> 22222222
+      git log for #{old_os_staging_rev}..#{new_rev}
+      > 222222222 os staging change
+    MSG
   end
 
   it 'does not include the revision in title for changes from different sources' do
